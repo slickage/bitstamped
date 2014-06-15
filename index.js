ba5249616bb3736f437aca0a1952b11a4dd2a42a
Anthony Kinsey <anthony@slickage.com>
2014-06-14 20:20:29 -1000

    Added init, commented out express

--- index.js
+++ index.js
@@ -1,33 +1,15 @@
 var request = require('request');
 var nano = require('nano')('http://localhost:5984');
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
+// var express = require('express');
+// var bodyParser = require('body-parser');
+// var app = express();
 var couchapp = require('couchapp');
 var ddoc = require('./couchapp');
 var dbName = 'bitstamped';
-app.use(bodyParser());
-
+// app.use(bodyParser());
 var db;
-nano.db.get(dbName, function(err) {
-  if (err) {
-    nano.db.create(dbName, function(err) {
-      if (err) {
-        return process.exit(1);
-      }
-      db = nano.use(dbName);
-      var dbUrl = 'http://localhost:5984/' + dbName;
-      couchapp.createApp(ddoc, dbUrl, function(app) {
-        app.push();
-      });
-    });
-  }
-  else {
-    db = nano.use(dbName);
-  }
-});
 
-var requestBitstampData = function() {
+function requestBitstampData() {
   request('https://www.bitstamp.net/api/ticker/', function (error, response, body) {
     if (!error && response.statusCode === 200) {
       var tickerData = JSON.parse(body);
@@ -38,33 +20,54 @@ var requestBitstampData = function() {
       db.insert(tickerData);
     }
   });
-};
-
-var interval = 1000 * 60;
-
-setInterval(requestBitstampData, interval);
+}
 
 var getTicker = function(timestamp, cb) {
   timestamp = Number(timestamp) / 1000; // discard milliseconds
   db.view('bitstamped', 'tickerByTime', { limit: 1, descending: true, startkey: timestamp }, cb);
 };
 
-var routes = function(app) {
-  app.get('/api/ticker/:timestamp', function(req, res) {
-    getTicker(req.params.timestamp, function(err, body) {
-      if (!err) {
-        res.json(body.rows[0]);
-        res.end();
-      }
-      else {
-        res.json({ error: err });
-        res.end();
-      }
-    });
+// var routes = function(app) {
+//   app.get('/api/ticker/:timestamp', function(req, res) {
+//     getTicker(req.params.timestamp, function(err, body) {
+//       if (!err) {
+//         res.json(body.rows[0]);
+//         res.end();
+//       }
+//       else {
+//         res.json({ error: err });
+//         res.end();
+//       }
+//     });
+//   });
+// };
+
+ var init = function(dbRootUrl, interval) {
+    dbRootUrl = dbRootUrl || 'http://localhost:5984';
+    var reqInterval = interval || 1000 * 60;
+    nano.db.get(dbName, function(err) {
+    if (err) {
+      nano.db.create(dbName, function(err) {
+        if (err) {
+          console.log(err);
+          return process.exit(1);
+        }
+        db = nano.use(dbName);
+        couchapp.createApp(ddoc, dbRootUrl + dbName, function(app) {
+          app.push();
+          setInterval(requestBitstampData, reqInterval);
+        });
+      });
+    }
+    else {
+      db = nano.use(dbName);
+      setInterval(requestBitstampData, reqInterval);
+    }
   });
 };
 
 module.exports = {
-  routes: routes,
+  init: init,
+//  routes: routes,
   getTicker: getTicker
  };
