33ba7a73bdb6e9fb8c74d0c07a79026ef6890015
Anthony Kinsey <anthony@slickage.com>
2014-06-16 12:36:39 -1000

    adjusting route path. deleting unneeded fields

--- index.js
+++ index.js
@@ -23,10 +23,14 @@ var getTicker = function(timestamp, cb) {
   db.view('bitstamped', 'tickerByTime', { limit: 1, descending: true, startkey: timestamp }, cb);
 };
 
-api.route('/api/ticker/:timestamp').get(function(req, res) {
+api.route('/bistamped/:timestamp').get(function(req, res) {
   getTicker(req.params.timestamp, function(err, body) {
     if (!err) {
-      res.json(body.rows[0]);
+      var tickerData = body.rows[0].value;
+      delete tickerData._id;
+      delete tickerData._rev;
+      delete tickerData.type;
+      res.json(tickerData);
       res.end();
     }
     else {
